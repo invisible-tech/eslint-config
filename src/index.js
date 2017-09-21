8785f00f42a98ff9d9887987a65bbf31e7a16e9a
rodrigo4244 <rodrigo4244@users.noreply.github.com>
2017-09-21 08:49:39 -0300

    feat: invisible eslint config

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-#!/usr/bin/env node
-
 'use strict'
 
-const spawn = require('cross-spawn')
-
-const cwd = process.cwd()
-
-const configFilePath = `${cwd}/node_modules/inv-lint/.eslintrc.json`
-const outputFormatPath = `${cwd}/node_modules/eslint-formatter-pretty`
-const filesPath = [`${cwd}/scripts`, `${cwd}/src`, `${cwd}/test`, '--ignore-path', `${cwd}/.gitignore`]
-
-const eslintCommand = ['-c', configFilePath, '--cache', '-f', outputFormatPath, ...filesPath]
-const fixCommand = ['-c', configFilePath, '--cache', '--fix', ...filesPath]
-
-spawn.sync('eslint', eslintCommand, { stdio: 'inherit' })
-spawn.sync('eslint', fixCommand, { stdio: 'inherit' })
+module.exports = {
+  extends: [
+    './rules/invRules.js',
+  ].map(require.resolve),
+  rules: {},
+}
